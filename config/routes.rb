cfa53a0aaa65b8967e0eab261283b76f9fdf4fc4
Glen Warren <glenwarren@zoho.com>
2019-08-08 17:21:22 +0200

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  resources :ingredients, only: [:index, :show]
+  resources :doses, only: [:show, :edit, :update, :destroy]
 end
